3476779dec70b83bc9931a121049421586ca5ff5
Jkrisha Gail Flores <jkrishagailflores@gmail.com>
2026-01-14 05:32:49 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,10 +5,10 @@
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  projectId: "studio-227719239-efe98",
+  appId: "1:630277961426:web:bb3c1083c72518abf4df0b",
+  apiKey: "AIzaSyAmsBIjLGpCofUcV5M5aIiWBMqrqu6w2rg",
+  authDomain: "studio-227719239-efe98.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "630277961426"
 };
